0c435f91b2724c6ed4d4933373c785885e9937c9
spacedmi <spacedmi@gmail.com>
2014-07-15 16:35:32 +0400

    Added some code for .AVI file record (record MainAVIHeader, StreamHeader)

--- mjpegwriter.cpp
+++ mjpegwriter.cpp
@@ -1,18 +1,67 @@
+/* СТРУКТУРЫ ЗАГОЛОВКОВ
+MainAVIHeader:
+DWORD dwMicroSecPerFrame;    //  Количество микросекунд между кадрами для всего файла
+DWORD dwMaxBytesPerSec;      //  Примерную максимальную скорость передачи данных файла
+DWORD dwReserved1;           // NULL
+DWORD dwFlags;               // ??
+DWORD dwTotalFrames;         // Общее число кадров, запишется в самом конце
+DWORD dwInitialFrames;       // Игнорировать
+DWORD dwStreams;             // Число потоков 2: Видео + аудио
+DWORD dwSuggestedBufferSize; // Размер буфера для большего chunka'а. Если 0, то буфер задаст программа для воспроизведения
+DWORD dwWidth;               // Ширина кадра
+DWORD dwHeight;              // Высота кадра
+DWORD dwReserved[4];         // NULL, NULL, NULL, NULL
+AVIStreamHeader:
+FOURCC fccType;              // Может иметь fourcc 'vids','auds', 'txts'...
+FOURCC fccHandler;           // cvid 
+DWORD dwFlags;               // NULL
+DWORD dwPriority;            // NULL
+DWORD dwInitialFrames;       // NULL
+DWORD dwScale;               // NULL
+DWORD dwRate;                // NULL
+DWORD dwStart;               // NULL
+DWORD dwLength;              // NULL
+DWORD dwSuggestedBufferSize; // NULL
+DWORD dwQuality;             // NULL
+DWORD dwSampleSize;          // NULL
+RECT rcFrame;
+*/
 #include "mjpegwriter.hpp"
 #include <fstream>
 
-MjpegWriter::MjpegWriter() : isOpen(false), outFile(NULL), outformat(1), outfps(20) { }
+typedef unsigned long DWORD;
+typedef unsigned char BYTE;
+typedef DWORD FOURCC;
+typedef unsigned short WORD;
+#define fourCC(a,b,c,d) ( (DWORD) (((d)<<24) | ((c)<<16) | ((b)<<8) | (a)) )
+#define NUM_MICROSEC_PER_SEC 1000000
+#define MAX_BYTES_PER_SEC 314572800         // 300 Mb/s
+#define STREAMS 1
+
+MjpegWriter::MjpegWriter() : isOpen(false), outFile(NULL), outformat(AVI), outfps(20) { }
 
 int MjpegWriter::Open(const char* outfile, uchar format, uchar fps)
 {
+    if (fps < 1) return -3;
     switch (format)
     {
-        case 1:                 // if AVI
-            if (outFile = fopen(outfile, "wb+"))
+        case AVI:
+        {
+            if (!(outFile = fopen(outfile, "wb+")))
                 return -1;
-            // Здесь запись заголовка файла
+            // Record MainAviHeader. НЕ ЗАБЫТЬ ПОТОМ ЗАПИСАТЬ РАЗМЕР
+            DWORD StartAVIRIFF[22] = { fourCC('R', 'I', 'F', 'F'), NULL, fourCC('A', 'V', 'I', ' '),
+                fourCC('L', 'I', 'S', 'T'), NULL, fourCC('h', 'd', 'r', 'l'), fourCC('a', 'v', 'i', 'h'),
+                sizeof(DWORD) * 14, 1.0f * NUM_MICROSEC_PER_SEC / fps, MAX_BYTES_PER_SEC, NULL,
+                NULL, NULL, NULL, STREAMS, NULL, NULL, NULL, NULL, NULL, NULL, NULL };
+            fwrite(StartAVIRIFF, sizeof(DWORD), 22, outFile);
+            // Record StreamHeader. НЕ ЗАБЫТЬ ПОТОМ ЗАПИСАТЬ РАЗМЕР
+            DWORD StreamHEAD[18] = { fourCC('L', 'I', 'S', 'T'), NULL, fourCC('s', 't', 'r', 'l'), fourCC('s', 't', 'r', 'h'),
+                sizeof(DWORD)* 13 + sizeof(WORD), fourCC('v', 'i', 'd', 's'), fourCC('c', 'v', 'i', 'd'), NULL, NULL, NULL,
+            NULL, NULL, NULL, NULL, NULL, NULL, NULL};
             outfps = fps;
             break;
+        }
         default: 
             return -2;
     }
@@ -37,6 +86,7 @@ int MjpegWriter::Write(const Mat Im)
     // Потоковая запись, тут вызывается jpeg энкодер
     return 1;
 }
+
 bool MjpegWriter::isOpened()
 {
     return isOpen;
--- mjpegwriter.hpp
+++ mjpegwriter.hpp
@@ -1,5 +1,6 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
+#include <intsafe.h>
 #include <ctype.h>
 
 #define AVI 1
@@ -12,10 +13,11 @@ class MjpegWriter
 public:
     FILE * outFile;
     uchar outformat, outfps;
+    uint width, height;
     MjpegWriter();
     int Open(const char* outfile, uchar format, uchar fps);
-    int Close();
     int Write(const Mat Im);
+    int Close();
     bool isOpened();
 private:
     bool isOpen;
